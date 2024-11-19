ac4009fff2706530ed1be1bb34aa687424223462
Osvaldo <osvaldobm@seseqro.gob.mx>
2024-11-19 10:22:47 -0600

    Se cambio a formato ancho la tabla de datos de productividad y se validó el funcionamiento de los filtros.

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def cargar_datos():
     try:
         # Lee el archivo Excel
         df = pd.read_excel(ruta_excel, sheet_name='Hoja1')  # Cambia 'Datos' por el nombre de tu hoja si es necesario
+        df['Fecha']=pd.to_datetime(df['Fecha'], errors='coerce')  # Asegúrate de que la columna 'Fecha' esté en el formato correcto
         print("Datos cargados con éxito:")
         print(df.head())
         return df #devuelve el data frame df
@@ -21,6 +22,8 @@ def cargar_datos():
 def calcular_productividad_periodo(datos, meses):
     # Ordenar por la fecha para que las agrupaciones sean cronológicas
     datos = datos.sort_values(by='Fecha')
+    # Asegurarse de que la columna 'Fecha' esté en formato datetime
+    datos['Fecha'] = pd.to_datetime(datos['Fecha'], errors='coerce')
 
     # Definir frecuencia de agrupación según el periodo de meses
     if meses == 2:
@@ -29,27 +32,26 @@ def calcular_productividad_periodo(datos, meses):
         freq = '6M'  # Semestral
     else:  # Por defecto, si no se especifica un periodo válido, no se agrupa
       # Productividad sin agrupación (ningún periodo seleccionado)
-        datos['Productividad'] = datos['Consultas de unidad'] / datos['JornadasxUnidad']
-
-    if not freq:
-        # Si no se especifica un periodo válido, devolver datos sin cambios
-        datos['Productividad'] = datos['Consultas de unidad'] / datos['JornadasxUnidad']
-        return datos
+        datos['Productividad'] = datos['Consultas de unidad'] / datos['JornadasxUnidad'].round(2)  # Calcular productividad sin agrupar
+        return datos    
 
     # Agrupar por 'Clues', 'Unidad' y periodos de la columna 'Fecha'
     datos_agrupados = datos.groupby(
-        ['Clues', 'Unidad', pd.Grouper(key='Fecha', freq=freq)]
+        ['Clues', 'Unidad', pd.Grouper(key='Fecha', freq=freq), 'Año']
     ).agg({
         'Consultas de unidad': 'sum',
         'JornadasxUnidad': 'sum'
     }).reset_index()
 
-    # Cálculo de productividad
-    datos_agrupados['Productividad'] = datos_agrupados['Consultas de unidad'] / datos_agrupados['JornadasxUnidad']
+     # Verifica si los datos agrupados son correctos
+    print("Datos agrupados:")
+    print(datos_agrupados.head())
+
+# Cálculo de productividad
+    datos_agrupados['Productividad'] = (datos_agrupados['Consultas de unidad'] / datos_agrupados['JornadasxUnidad']).round(2)  # Calcular productividad agrupada
     # Ordenar los resultados por fecha de forma descendente
     datos_agrupados = datos_agrupados.sort_values(by='Fecha', ascending=False)
     return datos_agrupados
-
 
 
 @app.route('/')
@@ -83,11 +85,12 @@ def datos():
     tipologia_unica = datos['Tipologia'].unique().tolist()  # Obtener tipologías únicas
     turnos_unicos = datos['Turno'].unique().tolist()  # Obtener turnos únicos   
     trimestre_unico = datos['Trimestre'].unique().tolist()  # Obtener trimestres únicos
-    anio_unico = datos['Año'].unique().tolist()  # Obtener años únicos  
+    anio_unicos = datos['Año'].unique().tolist()  # Obtener años únicos  
 
     print("Municipios únicos:", municipios_unicos)  # Imprimir municipios únicos para depuración
     print("Unidades únicas:", unidades_unicas)  # Imprimir unidades únicas para depuración
     print("Meses únicos:", meses_unicos)  # Imprimir meses únicos para depuración
+    print("Años unicos", anio_unicos)
 
      # Inicializar variables de filtro con valores predeterminados o None    
     municipio = request.form.get('municipio') if request.method == 'POST' else None    
@@ -107,8 +110,8 @@ def datos():
         datos = datos[datos['Municipio'] == municipio]
     if unidad:
         datos = datos[datos['Unidad'] == unidad]
-    if mes:
-        datos = datos[datos['Mes'] == mes]
+    if anio:
+        datos = datos[datos['Año'] == int(anio)]
     
        # Calcular la productividad en función del periodo seleccionado
     if periodo == "2":  # Agrupación bimestral
@@ -117,9 +120,10 @@ def datos():
         datos = calcular_productividad_periodo(datos, meses=6)
     else:
         # Productividad sin agrupación (ningún periodo seleccionado)
-        datos['Productividad'] = datos['Consultas de unidad'] / datos['JornadasxUnidad']
+        datos['Productividad'] = datos['Consultas de unidad'] / datos['JornadasxUnidad'].round(2)  # Calcular la productividad sin agrupación
 
-
+    # Transformar los datos a formato de mes
+    datos = transformar_a_mes(datos)
     
 #    datos['Productividad']=datos['Productividad'].astype(float)  # Convertir la columna 'Productividad' a tipo float
 #    datos['Año' ] = datos['Fecha'].dt.year  # Extraer el año de la columna 'Fecha' y crear una nueva columna 'Año'
@@ -133,13 +137,67 @@ def datos():
                            tipologia_unica=tipologia_unica,
                            turnos_unicos=turnos_unicos,
                            trimestre_unico=trimestre_unico,
-                           anio_unicos=anio_unico)
+                           anio_unicos=anio_unicos)
 
 
     #if request.method == 'POST':
 
  #   lista_datos = datos.to_dict(orient='records')  # Convertir a lista de diccionarios
  #   return render_template('datos.html', datos=lista_datos)
+
+# Transformar los datos a formato de mes
+def transformar_a_mes(datos):
+    meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+    
+    datos['Mes'] = datos['Fecha'].dt.month
+    datos_agrupados_mes = datos.pivot_table(
+        index=['Clues', 'Unidad', 'Año'],
+        columns='Mes',
+        values='Productividad',
+        aggfunc='first'
+    ).reset_index()
+
+    # Verifica si los datos agrupados por mes están correctos
+    print("Datos agrupados por mes:")
+    print(datos_agrupados_mes.head())
+
+
+        # Renombrar las columnas numéricas de meses a nombres de meses
+    column_mapping = {i + 1: meses[i] for i in range(12)}  # Mapear 1-12 a nombres de meses
+    datos_agrupados_mes = datos_agrupados_mes.rename(columns=column_mapping)
+
+# Formatear las columnas de los meses para que tengan un decimal
+    for mes in meses:
+        if mes in datos_agrupados_mes.columns:
+            datos_agrupados_mes[mes] = datos_agrupados_mes[mes].apply(
+                lambda x: f"{float(x):.1f}" if isinstance(x, (int, float)) else x
+            )
+
+     # Asegurarse de que todas las columnas de meses estén presentes
+    for mes in meses:
+        if mes not in datos_agrupados_mes.columns:
+            datos_agrupados_mes[mes] = None  # Agregar columna faltante con valores 'None'
+
+
+# Si la tabla tiene más o menos de 12 columnas (más las 3 de 'Clues', 'Unidad' y 'Año'), ajusta
+    if len(datos_agrupados_mes.columns) == 15:  # Tres columnas más los 12 meses
+        # Renombrar las columnas para que sean los nombres de los meses
+        datos_agrupados_mes.columns = ['Clues', 'Unidad', 'Año'] + meses
+    else:
+        # Ajusta según lo que necesites si el número de columnas no es 15
+        print("Error: El número de columnas no es el esperado.")
+
+        
+  # Asegurarse de que todas las columnas de meses estén presentes, incluso si faltan en los datos
+    for mes in meses:
+        if mes not in datos_agrupados_mes.columns:
+            datos_agrupados_mes[mes] = None  # Agregar columna faltante con valores 'None'
+  # Rellenar los valores faltantes con "N/A" para visualización en HTML
+    datos_agrupados_mes = datos_agrupados_mes.fillna("N/A")
+
+
+
+    return datos_agrupados_mes
 
 
 if __name__ == '__main__':
